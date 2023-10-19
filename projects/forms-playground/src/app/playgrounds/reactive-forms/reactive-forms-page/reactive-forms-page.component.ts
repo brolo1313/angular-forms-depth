521da82445b1889b32d1a219e64881fac3dfe5a1
Yupin Andriy <brolo1341@gmail.com>
2023-10-19 20:23:27 +0300

    added async validator

--- projects/forms-playground/src/app/playgrounds/reactive-forms/reactive-forms-page/reactive-forms-page.component.ts
+++ projects/forms-playground/src/app/playgrounds/reactive-forms/reactive-forms-page/reactive-forms-page.component.ts
@@ -1,10 +1,11 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormArray, FormBuilder, FormControl, FormGroup, FormRecord, ReactiveFormsModule, Validators } from '@angular/forms';
-import { Observable, tap } from 'rxjs';
+import { Observable, Subscription, bufferCount, filter, tap } from 'rxjs';
 import { UserSkillsService } from '../../../core/user-skills.service';
 import { banWords } from '../validators/ban-word.validator';
 import { passwordShouldMatch } from '../validators/password-match.validator';
+import { UniqueNicknameValidator } from '../validators/uniq-name.validator';
 
 @Component({
   selector: 'app-reactive-forms-page',
@@ -28,11 +29,18 @@ export class ReactiveFormsPageComponent implements OnInit {
     firstName: ['Andriy', [Validators.required, Validators.minLength(2), banWords(['test', 'dummy'])]],
     lastName: ['Yupin', [Validators.required, Validators.minLength(2)]],
     nickname: ['',
-    [
-      Validators.required,
-      Validators.minLength(2),
-      Validators.pattern(/^[\w.]+$/)
-    ]
+    {
+      validators: [
+        Validators.required,
+        Validators.minLength(2),
+        Validators.pattern(/^[\w.]+$/),
+        banWords(['dummy', 'anonymous'])
+      ],
+      asyncValidators: [
+        this.uniqueNickname.validate.bind(this.uniqueNickname)
+      ],
+      updateOn: 'blur'
+    },
   ],
     email: ['brolo1341@gmail.com', [Validators.email, Validators.required]],
     yearOfBirth: this.fb.nonNullable.control(
@@ -58,15 +66,24 @@ export class ReactiveFormsPageComponent implements OnInit {
     }, { validators: passwordShouldMatch})
   });
 
+  private formPendingState!: Subscription;
+  
   constructor(
     private userSkills: UserSkillsService,
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private uniqueNickname: UniqueNicknameValidator,
+    private cd: ChangeDetectorRef
   ) { }
 
   ngOnInit(): void {
     this.skills$ = this.userSkills.getSkills().pipe(
       tap(skills => this.buildSkillControls(skills))
     );
+
+    this.formPendingState = this.form.statusChanges.pipe(
+      bufferCount(2, 1),
+      filter(([prevState]) => prevState === 'PENDING')
+    ).subscribe(() => this.cd.markForCheck())
   }
 
   addPhone() {

41393c981e40d431089b9be24f2da4a782d26c9f
Yupin Andriy <brolo1341@gmail.com>
2023-10-24 17:09:47 +0300

    restore the Form from config

--- projects/forms-playground/src/app/playgrounds/dynamic-forms/dynamic-forms-page/dynamic-forms-page.component.ts
+++ projects/forms-playground/src/app/playgrounds/dynamic-forms/dynamic-forms-page/dynamic-forms-page.component.ts
@@ -1,12 +1,14 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Observable, Subject, switchMap } from 'rxjs';
+import { Observable, Subject, switchMap, tap } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { DynamicFormConfig } from '../models/dynamic-forms.models';
 
 @Component({
   selector: 'app-dynamic-forms-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './dynamic-forms-page.component.html',
   styleUrls: [
     '../../common-page.scss',
@@ -16,15 +18,28 @@ import { HttpClient } from '@angular/common/http';
 })
 export class DynamicFormsPageComponent implements OnInit {
 
+  
+  form!: FormGroup;
+
   protected formLoadingTrigger = new Subject<'user' | 'company'>();
-  protected formConfig$!: Observable<object>;
+  protected formConfig$!: Observable<DynamicFormConfig>;
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.formConfig$ = this.formLoadingTrigger.pipe(
-      switchMap(config => this.http.get(`assets/${config}.form.json`))
+      switchMap(config => this.http.get<DynamicFormConfig>(`assets/${config}.form.json`)),
+      tap(({ controls }) => this.buildForm(controls))
     );
   }
-
+  protected onSubmit() {
+    console.log('Submitted data: ', this.form.value);
+    this.form.reset();
+  }
+  private buildForm(controls: DynamicFormConfig['controls']) {
+    this.form = new FormGroup({});
+    Object.keys(controls).forEach(key => {
+      this.form.addControl(key, new FormControl(controls[key].value));
+    });
+  }
 }

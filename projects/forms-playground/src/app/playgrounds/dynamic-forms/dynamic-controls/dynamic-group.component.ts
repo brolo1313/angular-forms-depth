34dfdd4cb1cf16196b558565bac228d374482940
Yupin Andriy <brolo1341@gmail.com>
2023-11-05 12:15:09 +0200

    improved HTML structure for Dynamic Form

--- projects/forms-playground/src/app/playgrounds/dynamic-forms/dynamic-controls/dynamic-group.component.ts
+++ projects/forms-playground/src/app/playgrounds/dynamic-forms/dynamic-controls/dynamic-group.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, HostBinding, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BaseDynamicControl } from './base-dynamic-control';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -13,11 +13,12 @@ import { DynamicControlResolver } from '../dynamic-control-resolver.service';
     <ng-container [formGroup]="formGroup">
       <fieldset [formGroupName]="control.controlKey">
         <legend>{{control.config.label}}</legend>
-        <div *ngFor="let control of control.config.controls | keyvalue" class="form-field">
-        <ng-container
-          [ngComponentOutlet]="controlResolver.resolve(control.value.controlType)"
-          [ngComponentOutletInjector]="control.key | controlInjector:control.value"></ng-container>
-        </div>
+        <ng-container *ngFor="let control of control.config.controls | keyvalue" class="form-field">
+          <ng-container
+            [ngComponentOutlet]="controlResolver.resolve(control.value.controlType)"
+            [ngComponentOutletInjector]="control.key | controlInjector:control.value">
+          </ng-container>
+        </ng-container>
       </fieldset>
     </ng-container>
   `,
@@ -25,5 +26,7 @@ import { DynamicControlResolver } from '../dynamic-control-resolver.service';
   ]
 })
 export class DynamicGroupComponent extends BaseDynamicControl {
+  @HostBinding('class') override hostClass = 'form-field-group';
+
   controlResolver = inject(DynamicControlResolver);
 }
